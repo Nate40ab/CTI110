34c1879893fd36239aea0c7f2cc8ab7dfdbf271e
Nate40ab <chathamn17@gmail.com>
2026-09-17 19:11:23 +0000

    uploading file

---
+++ P1LAB1_chathamnathan.py
@@ -0,0 +1,9 @@
+# P1LAB1
+# Nathan Chatham
+# p1lab1.py
+# input and print
+
+first_name = input("enter first name: ")
+last_name = input("enter last name: ")
+
+print("Hello,",first_name,last_name +"!","welcome to CTI-110!")
